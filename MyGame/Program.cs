2c3798d23548971d4619b82028e4a126a4ee0b63
Alice <alicelopess.art@gmail,com>
2025-03-27 20:24:29 +0000

    metodos adicionados

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -17,5 +17,22 @@ namespace MyGame
         public static void Main()
         {
         }
+
+          public string GetName()
+        {
+            return name;
+        }
+        public void TakeDamage(float damage)
+        {
+            shield -= (int)damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= (int)damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+            
+        }
     }
 }
