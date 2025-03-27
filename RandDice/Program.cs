e2183df30d31adaa0dcfa9e24bb847ad55d9fb2c
Alice <alicelopess.art@gmail,com>
2025-03-27 20:02:38 +0000

    exercicio cinco

---
+++ RandDice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class random
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine(" ");
+            return;
+        }
+
+        if (!int.TryParse(args[0], out int n) || !int.TryParse(args[1], out int s))
+        {
+            Console.WriteLine("Erro.");
+            return;
+        }
+
+        Random random = new Random(s);
+        int soma = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            soma += random.Next(1, 7); 
+        }
+
+        Console.WriteLine(soma);
+    }
+}
