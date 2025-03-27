cb96f66fa70331287f1ea857a64b24a0e9bd1afe
Alice <alicelopess.art@gmail,com>
2025-03-27 20:15:01 +0000

    construtor da classe Enemy

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -4,9 +4,18 @@ namespace MyGame
 {
     public class Enemy
     {
+        private string name;
+        private int health;
+        private int shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
         public static void Main()
         {
-
         }
     }
 }
